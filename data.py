8a1e8e4d7ee84a639e600a4f5db88db49ded1fa7
David Garcia <davidgarciabarrios@gmail.com>
2024-07-14 00:14:39 -0500

    Project

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "The Halting Problem can be solved by a Turing machine.", "answer": "False"},
+    {"text": "The P vs NP problem is considered solved in theoretical computer science.", "answer": "False"},
+    {"text": "In a binary search tree, the left subtree of a node contains only nodes with keys greater than the node's key", "answer": "False"},
+    {"text": "In the context of database management systems ACID stands for Atomicity Consistency Isolation and Durability.", "answer": "True"},
+    {"text": "Moore's Law states that the number of transistors on a microchip doubles every year.", "answer": "False"},
+    {"text": "A context-free grammar can be used to describe all possible programming languages.", "answer": "False"},
+    {"text": "The complexity class NP contains all problems that can be verified in polynomial time.", "answer": "True"},
+    {"text": "In cryptography, RSA encryption is based on the difficulty of factoring large prime numbers.", "answer": "True"},
+    {"text": "The time complexity of the Quicksort algorithm in the worst case is O(n log n).", "answer": "False"},
+    {"text": "In a relational database, a foreign key is a primary key in another table.", "answer": "True"},
+    {"text": "The Traveling Salesman Problem can be solved in polynomial time using dynamic programming.", "answer": "False"},
+    {"text": "In computer networks, TCP is a connection-oriented protocol, while UDP is a connectionless protocol.", "answer": "True"}
+]
